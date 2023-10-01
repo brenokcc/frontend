45c548b3e18617ae150ab68a1505bf55debc7cee
Breno Silva <brenokcc@yahoo.com.br>
2023-10-01 05:55:52 -0300

    Continuação da implementação

--- src/lib/Dashboard.jsx
+++ src/lib/Dashboard.jsx
@@ -1,4 +1,5 @@
 import {Icon, TitleCase} from './Utils'
+import Action from './Action'
 
 function Boxes(props){
     return (
@@ -40,10 +41,31 @@ function Indicators(props){
     )
 }
 
+function Warning(props){
+    return (
+        <div className="warning">
+            <div className="icon">
+                <Icon icon="warning"/>
+            </div>
+            <div className="detail">
+                 <div className="text">
+                    <strong>Atenção</strong>
+                    A conclusão de sua capacitação ainda não foi registrada na plataforma! Algumas funcionalidades tais como correção e monitoramento de correção de inconsistências só estarão disponíveis após a realização da capacitação
+                 </div>
+                 <div className="actions">
+                    <Action href="#">Realizar Capacitação</Action>
+                    <Action href="#">Confirmar Capacitação</Action>
+                 </div>
+            </div>
+        </div>
+    )
+}
+//<div>{JSON.stringify(props.data)}</div>
 function Dashboard(props){
     return (
         <>
-        <div>{JSON.stringify(props.data)}</div>
+
+        <Warning/>
         <Boxes title="acesso_rapido" data={props.data.result.acesso_rapido}/>
         <Indicators title="percentual_resolucao_inconsistencia" data={props.data.result.percentual_resolucao_inconsistencia}/>
         </>
